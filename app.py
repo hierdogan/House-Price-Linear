c052e4cd9f04cdd42ea0dd85af2e9748f4fbf94d
hierdogan <ibrahimerdogan@gmail.com>
2023-10-10 21:53:22 +0300

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+from xgboost import XGBRegressor
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 import pickle
 
 st.set_page_config(
